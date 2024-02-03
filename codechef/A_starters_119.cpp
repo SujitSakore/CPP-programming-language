e6a9ad9da1ad81065094e58beace16e44e5aff7d
Sujit Sampat Sakore <150163338+SujitSakore@users.noreply.github.com>
2024-02-04 00:27:49 +0530

    Add files via upload

---
+++ codechef/A_starters_119.cpp
@@ -0,0 +1,18 @@
+// Advitiya
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int sujit;
+    cin>>sujit;
+    if(sujit>=16 && sujit<=18){
+        cout<<"ADVITIYA"<<endl;    
+    }else{
+        cout<<"WAITING FOR ADVITIYA"<<endl;
+    }
+    return 0;
+    
+}
