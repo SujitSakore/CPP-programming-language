52b1cbd2f1bcfc6ffc19b01d9594697d7709adb6
Sujit Sampat Sakore <150163338+SujitSakore@users.noreply.github.com>
2024-02-11 01:12:29 +0530

    Add files via upload

---
+++ codechef/B_starters_120.cpp
@@ -0,0 +1,22 @@
+//  Valentine Gifts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int sujit;
+    cin>>sujit;
+    while(sujit !=0){
+        int sakore;
+        cin>>sakore;
+        if (sakore>=127){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        }
+        sujit--;
+    }
+    return 0;
+}
+
