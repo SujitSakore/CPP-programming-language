256f2bb100f3d9505a4b7ed1611134653a1f943c
Sujit Sampat Sakore <150163338+SujitSakore@users.noreply.github.com>
2024-02-16 01:18:30 +0530

    Add files via upload

--- codechef/B_starters_120.cpp
+++ codechef/B_starters_120.cpp
@@ -1,22 +1,25 @@
-//  Valentine Gifts
+//  Sub or Swp
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	// your code goes here
+int gcd(int a, int b){
+    while(b!=0){
+        int temp =b;
+        b=a%b;
+        a=temp;
+    }
+    return a;
+}
+int main(){
     int sujit;
     cin>>sujit;
-    while(sujit !=0){
-        int sakore;
-        cin>>sakore;
-        if (sakore>=127){
-            cout<<"YES"<<endl;
-        }else{
-            cout<<"NO"<<endl;
-        }
-        sujit--;
+    while(sujit--){
+        int x,y;
+        cin>>x>>y;
+        cout<<gcd(x,y)<<endl;
     }
     return 0;
 }
+
 
