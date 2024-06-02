d4d5eb285a145d7cba45a615791ecce031d3c7d2
Sujit Sampat Sakore <150163338+SujitSakore@users.noreply.github.com>
2024-06-02 23:40:09 +0530

    Create Basic_Data_Types.cpp

---
+++ Hacker Rank/Basic_Data_Types.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a;
+    long long1;
+    char c;
+    float f;
+    double d;
+    cin>>a;
+    cin>>long1;
+    cin>>c;
+    cin>>f;
+    cin>>d;
+    cout << setprecision(20) << a<< endl;
+    cout << setprecision(20) << long1<< endl;
+    cout << setprecision(20) << c<< endl;
+    cout << setprecision(20) << f<< endl;
+    cout << setprecision(20) << d<< endl;
+    return 0;
+}
